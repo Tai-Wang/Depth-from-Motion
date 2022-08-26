777a796843a9406ec97678ca54c6e9843ea8ea5d
Tai-Wang <tab_wang@outlook.com>
2022-08-26 19:09:02 +0800

    Add w/o distnorm config for more balanced 3-class results and photometric distortion for a little more stable performance

--- tools/dfm.sh
+++ tools/dfm.sh
@@ -3,15 +3,15 @@
 set -x
 
 CKPT_PATH=/mnt/lustre/wangtai/mmdet3d-prerelease/work_dirs
-PARTITION=mm_det
-JOB_NAME=dfm-repro-benchmark-wodistnorm-v2
-TASK=dfm-repro-benchmark-wodistnorm-v2
-CONFIG=configs/dfm/dfm_r34_1x8_kitti-3d-3class_wodistnorm.py
+PARTITION=robot
+JOB_NAME=photodist-v8
+TASK=photodist-v8
+CONFIG=configs/dfm/dfm_r34_1x8_kitti-3d-3class_photodist.py
 WORK_DIR=${CKPT_PATH}/${TASK}
 CKPT=${CKPT_PATH}/${TASK}/latest.pth
 GPUS=8
 GPUS_PER_NODE=8
-XNODE=SH-IDC1-10-140-0-[131,137,168,230,246],SH-IDC1-10-140-1-[61,104]
+XNODE=SH-IDC1-10-140-0-[131,137,168,230,246],SH-IDC1-10-140-1-[61,104,113]
 PORT=29301
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
